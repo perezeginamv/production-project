3ba7eea8d9382c8ef03baa39f4ff05469a2964ca
Marina <perezeginamv@mail.ru>
2025-11-16 18:35:18 +0500

    fix mistakes

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
-import ProfilePage from './Profilepage';
+import ProfilePage from './ProfilePage';
 
 export default {
     title: 'pages/ProfilePage',
